23e01bc774d9c620f0ec47fad10bd6e03a1a716c
Tifenn007 <104152068+Tifenn007@users.noreply.github.com>
2025-02-17 18:12:51 +0100

    API CRUD Pokemon

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import fs from 'fs';
+import fs, { stat } from 'fs';
 import path from "path";
 import { fileURLToPath } from "url";
 import dotenv from "dotenv";
@@ -52,6 +52,71 @@ app.get("/api/pokemons", (req, res) => {
     ],
     pokemons: pokemonsList,
   });
+});
+
+app.get("/api/pokemons/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const pokemon = pokemonsList.find((pokemon) => pokemon.id === id);
+
+  if (pokemon) {
+    res.status(200).send({
+      status: 200,
+      data: pokemon
+    });
+  } else {
+    res.status(404).send({
+      status: 404,
+      message: "Pokemon non trouvé"
+    });
+  }
+});
+
+app.post("/api/newpok", (req, res) => {
+  const newPokemon = req.body;
+  pokemonsList.push(newPokemon);
+  fs.writeFileSync(path.join(__dirname, './data/pokemons.json'), JSON.stringify(pokemonsList), 'utf8');
+  res.status(201).send(newPokemon);
+});
+//uptade
+app.put("/api/pokemons/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const updatedPokemon = req.body;
+  const pokemonIndex = pokemonsList.findIndex((pokemon) => pokemon.id === id);
+
+  // Validation des données
+  if (!updatedPokemon.id || !updatedPokemon.name || !updatedPokemon.type || !updatedPokemon.base) {
+    return res.status(400).send({
+      status: 400,
+      message: "Les informations du Pokémon sont incomplètes"
+    });
+  }
+
+  if (pokemonIndex !== -1) {
+    pokemonsList[pokemonIndex] = updatedPokemon;
+    fs.writeFileSync(path.join(__dirname, './data/pokemons.json'), JSON.stringify(pokemonsList), 'utf8');
+    res.status(200).send(updatedPokemon);
+  } else {
+    res.status(404).send({
+      status: 404,
+      message: "Pokemon non trouvé"
+    });
+  }
+});
+
+app.delete("/api/pokemons/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const pokemonIndex = pokemonsList.findIndex((pokemon) => pokemon.id === id);
+
+  if (pokemonIndex !== -1) {
+    pokemonsList.splice(pokemonIndex, 1);
+    fs.writeFileSync(path.join(__dirname, './data/pokemons.json'), JSON.stringify(pokemonsList), 'utf8');
+    res.status(204).send();
+  } else {
+    res.status(404).send({
+      status: 404,
+      message: "Pokemon non trouvé"
+    });
+  }
 });
 
 app.get("/", (req, res) => {
